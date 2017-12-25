4462821c6b8aa279a11ddff5e69c918d00839d76
vladislavburch <vladislavburch17@gmail.com>
2017-12-25 05:47:13 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/vladislavburch/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    d72dd6336fb0e7312ce2cad5fda1a6b2daa10238
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
